95e00ced6e30ad89c84ddeaf5b7685d681ff909b
Muhammad Ali <info@muhammad-ali.uz>
2021-12-30 20:46:19 +0500

    replace digit of num

--- if09.py
+++ if09.py
@@ -1,6 +1,6 @@
 def main(a):
     """
-    The two-digit integer is given.
+    The two digit integer is given.
     Replace the digits of the number.
     True if the resulting number is less than or equal to the old number, otherwise return False.
     
@@ -9,4 +9,10 @@ def main(a):
     Returns:
         boolean: True if the resulting number is less than or equal to the old number, otherwise return False.
     """
-    return
+    a = str(a) 
+    b = a[1] + a[0]
+    if int(b) <= int(a):
+        return True
+    if int(b) > int(a):
+        return False
+
