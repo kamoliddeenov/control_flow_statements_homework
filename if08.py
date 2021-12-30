0c882e2ff3b0144d06bbabcbb3db186a3b64ca9a
Muhammad Ali <info@muhammad-ali.uz>
2021-12-30 20:54:41 +0500

    two/three digit condtions

--- if08.py
+++ if08.py
@@ -11,13 +11,13 @@ def main(a):
     Returns:
         string: the message to print
     """
-    if a > 99 and a % 2 == 1:
-        msg = "three-digit odd number"
-    if a > 99 and a % 2 == 0:
-        msg = "three-digit even number"
     if a > 9 and a % 2 == 1:
         msg = "two-digit odd number"
     if a > 9 and a % 2 == 0:
         msg = "two-digit even number"
+    if a > 99 and a % 2 == 1:
+        msg = "three-digit odd number"
+    if a > 99 and a % 2 == 0:
+        msg = "three-digit even number"
     
     return msg
